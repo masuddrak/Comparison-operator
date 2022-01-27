e6700182b66666dce6d0eab7e6ecd02a92a3ddf2
masudrana <masudranaacb@gmail.com>
2022-01-28 00:09:32 +0600

    conditional ses

--- Comparison-operator.js
+++ Comparison-operator.js
@@ -1,33 +1,8 @@
-// comparison operator
-
-// var myLove=100;
-// var yourLove=98;
-// console.log(myLove==yourLove);
-// console.log(myLove>yourLove);
-// console.log(myLove!=yourLove);
-
-// var iphonPrice=10000;
-// var myBlance=8000;
-// if(iphonPrice<myBlance){
-//     console.log("ami kint e parbo");
-// }
-// else{
-//     console.log("na ami ihpone kinbona")
-// }
-
 
 var gotJob=true;
 var saveMony=2000;
-if(gotJob==true && saveMony<2050){
-    console.log("ami kinte parbo dos");
-}else{
-    console.log("nare dos amar neoa hobena");
-}
-
-
-var gotJob=true;
-var saveMony=2000;
-if(gotJob==false && saveMony<2050){
+var delivaryTime=2;
+if((gotJob==true) && (saveMony>1550 || delivaryTime>3)){
     console.log("ami kinte parbo dos");
 }else{
     console.log("nare dos amar neoa hobena");
